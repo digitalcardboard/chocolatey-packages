9966f1e2c050e94ead5eb71417bf3c11c9c2db5f
Jason Navarrete <jason@digitalcardboard.com>
2016-03-21 15:46:06 -0700

    iperf3 3.1.2 (x64 release)

--- manual/iperf3/tools/chocolateyinstall.ps1
+++ manual/iperf3/tools/chocolateyinstall.ps1
@@ -2,9 +2,9 @@ $ErrorActionPreference = 'Stop';
 
 $packageName = 'iPerf3'
 $url = 'https://iperf.fr/download/iperf_3.0/iperf-3.0.11-win32.zip'
-$url64 = 'https://iperf.fr/download/iperf_3.0/iperf-3.0.11-win64.zip'
+$url64 = 'https://iperf.fr/download/iperf_3.1/iperf-3.1.2-win64.zip'
 $checksum = 'a726d7bb1a8b3fca367bf1597a7599d3e4439be0'
-$checksum64 = '276857e9569abb1176d2ac7910db3716b094bde1'
+$checksum64 = '7231d4433481aad147222d485a7a394e7f91be47'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
