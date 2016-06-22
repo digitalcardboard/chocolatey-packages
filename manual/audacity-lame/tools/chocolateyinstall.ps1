7a313737a882bf85fcb03c08208530644ed42330
Jason Navarrete <jason@digitalcardboard.com>
2016-06-22 10:56:08 -0700

    audacity-lame; iperf3 3.1.3

---
+++ manual/audacity-lame/tools/chocolateyinstall.ps1
@@ -0,0 +1,30 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+
+$packageName= 'audacity-lame'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://lame.buanzo.org/Lame_v3.99.3_for_Windows.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+  validExitCodes= @(0)
+
+  checksum      = 'a9aa7463aa9b685bdd20a5e12448c25b40a7677f'
+  checksumType  = 'sha1'
+}
+
+$options =
+@{
+  Headers = @{
+    Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
+    Referer = 'http://lame.buanzo.org/';
+  }
+}
+
+Install-ChocolateyPackage @packageArgs -Options $options
--- manual/iperf3/tools/chocolateyinstall.ps1
+++ manual/iperf3/tools/chocolateyinstall.ps1
@@ -1,12 +1,11 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'iPerf3'
-$url = 'https://iperf.fr/download/windows/iperf-3.1.2-win32.zip'
-$url64 = 'https://iperf.fr/download/windows/iperf-3.1.2-win64.zip'
-$checksum = 'e027f51b1a9952f4ca1dda43eeb803ca7ee202f8'
-$checksum64 = '7c2ffd5f448896e9a8416be9021e1bc22b06724c'
+$url = 'https://iperf.fr/download/windows/iperf-3.1.3-win32.zip'
+$url64 = 'https://iperf.fr/download/windows/iperf-3.1.3-win64.zip'
+$checksum = '36229bacfda9ad0a05d7eba9e3843b18b94046da'
+$checksum64 = 'c63d306daac82987839ed94cd8705a1121af0181'
 $checksumType = 'sha1'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -packageName "$packageName" -url "$url" -unzipLocation "$toolsDir" -checksum $checksum -checksumType $checksumType -url64bit "$url64" -checksum64 $checksum64
-
