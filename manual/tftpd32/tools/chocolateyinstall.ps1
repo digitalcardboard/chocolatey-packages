420fb78decc0399c3f375890d1056dd73400d814
Jason Navarrete <jason@digitalcardboard.com>
2017-04-10 12:11:04 -0700

    tftpd32/64 4.60

--- manual/tftpd32/tools/chocolateyinstall.ps1
+++ manual/tftpd32/tools/chocolateyinstall.ps1
@@ -2,18 +2,9 @@
 
 $packageName= 'tftpd32'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://tftpd32.jounin.net/download/tftpd32.452.zip'
-$url64      = 'http://tftpd32.jounin.net/download/tftpd64.452.zip'
-
-$packageArgs = @{
-  packageName   = $packageName
-  unzipLocation = $toolsDir
-  url           = $url
-  url64bit      = $url64
-  checksum      = 'd9cd272974692c3b5976a000bc0aafb1cba273b7'
-  checksumType  = 'sha1'
-  checksum64    = '07c52a972324d624f6dd9d2992f970bce165a8fc'
-  checksumType64= 'sha1'
+$fileLocation = $(Join-Path (Split-Path -parent $toolsDir) "application\tftpd32.460.zip")
+if (Get-ProcessorBits 64) {
+  $fileLocation = $(Join-Path (Split-Path -parent $toolsDir) "application\tftpd64.460.zip")
 }
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip -fileFullPath $fileLocation -destination $toolsDir
