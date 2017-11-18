6820e5a4dae5a1cacaa2a33f44738c340e3832fa
Jason Navarrete <jason@digitalcardboard.com>
2017-11-18 07:35:26 -0800

    lame 3.100

--- manual/lame/tools/chocolateyinstall.ps1
+++ manual/lame/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = 'LAME'
-$url = 'http://www.rarewares.org/files/mp3/lame3.99.5.zip'
-$url64 = 'http://www.rarewares.org/files/mp3/lame3.99.5-64.zip'
-$checksum = '5ec7274d605f5d11a8747c075c1daedcff87d629'
-$checksum64 = '0e1f1cdf5ddfd53c0e110c70250f8e9dd2acbaea'
-$checksumType = 'sha1'
+$url = 'http://www.rarewares.org/files/mp3/lame3.100.zip'
+$url64 = 'http://www.rarewares.org/files/mp3/lame3.100-64.zip'
+$checksum = '224d00e8e189d7d1cb904f6171e79b7b4f9564b7788cc00876b5a2a9d06ff5fe'
+$checksum64 = 'db810febfb1e338130903ddc33380b8f1566cdad5d1f006c04654facd0cc76f7'
+$checksumType = 'sha256'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage -packageName "$packageName" -url "$url" -unzipLocation "$toolsDir" -checksum $checksum -checksumType $checksumType -url64bit "$url64" -checksum64 $checksum64
