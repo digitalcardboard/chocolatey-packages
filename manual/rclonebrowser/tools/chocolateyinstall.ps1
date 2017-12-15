b6e30f935d366a60a596c3e1a3ba324790ec791c
Jason Navarrete <jason@digitalcardboard.com>
2017-12-15 13:57:28 -0800

    cleanup

--- manual/rclonebrowser/tools/chocolateyinstall.ps1
+++ manual/rclonebrowser/tools/chocolateyinstall.ps1
@@ -1,11 +1,7 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/mmozeiko/RcloneBrowser/releases/download/1.2/rclone-browser-1.2-a1156a0-win32.zip' # download url, HTTPS preferred
-$url64      = 'https://github.com/mmozeiko/RcloneBrowser/releases/download/1.2/rclone-browser-1.2-a1156a0-win64.zip' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://github.com/mmozeiko/RcloneBrowser/releases/download/1.2/rclone-browser-1.2-a1156a0-win32.zip'
+$url64      = 'https://github.com/mmozeiko/RcloneBrowser/releases/download/1.2/rclone-browser-1.2-a1156a0-win64.zip'
 
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
@@ -15,9 +11,9 @@ $packageArgs = @{
 
     softwareName = 'rclonebrowser*'
     checksum = 'c4114638e33bdaaac59c7265a80f4ceeecd3b3146f10b04aa4ded9ff6eaddc89'
-    checksumType = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+    checksumType = 'sha256'
     checksum64 = 'a982f563c086ac83645347abbc4cea39ffd40386101f05fa1c343d1397aea29d'
-    checksumType64 = 'sha256' #default is checksumType
+    checksumType64 = 'sha256'
 }
 
 $specificFolder = (Split-Path $url) -Replace ".zip"
@@ -25,5 +21,4 @@ if (Get-ProcessorBits 64) {
     $specificFolder = (Split-Path $url64) -Replace ".zip"
 }
 
-## Download and unpack a zip file - https://chocolatey.org/docs/helpers-install-chocolatey-zip-package
 Install-ChocolateyZipPackage $packageName $url $toolsDir $url64 -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64 -SpecificFolder $specificFolder
