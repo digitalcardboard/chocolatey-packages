026aa0e93aa86a1b9c95aacb54e972fafe0ce024
Jason Navarrete <jason@digitalcardboard.com>
2017-12-17 20:08:02 -0800

    https for audacity-lame

--- manual/audacity-lame/tools/chocolateyinstall.ps1
+++ manual/audacity-lame/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= 'audacity-lame'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://lame.buanzo.org/Lame_v3.99.3_for_Windows.exe'
+$url        = 'https://lame.buanzo.org/Lame_v3.99.3_for_Windows.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -15,15 +15,15 @@ $packageArgs = @{
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
 
-  checksum      = 'a9aa7463aa9b685bdd20a5e12448c25b40a7677f'
-  checksumType  = 'sha1'
+  checksum      = '90e9d629b4e528b77c6f51f185c3e81bb3a4eca7d6d53f049ed9ef7787464e8b'
+  checksumType  = 'sha256'
 }
 
 $options =
 @{
   Headers = @{
     Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-    Referer = 'http://lame.buanzo.org/';
+    Referer = 'https://lame.buanzo.org/';
   }
 }
 
