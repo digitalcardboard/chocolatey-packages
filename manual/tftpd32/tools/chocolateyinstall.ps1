30053563a8be21821e5da08de38e53b3454d73ec
Jason Navarrete <jason@digitalcardboard.com>
2020-09-02 12:15:41 -0700

    simplifing tftpd32 install routine

--- manual/tftpd32/tools/chocolateyinstall.ps1
+++ manual/tftpd32/tools/chocolateyinstall.ps1
@@ -1,10 +1,8 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName= 'tftpd32'
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = $(Join-Path (Split-Path -parent $toolsDir) "application\tftpd32.464.zip")
-if (Get-ProcessorBits 64) {
-  $fileLocation = $(Join-Path (Split-Path -parent $toolsDir) "application\tftpd64.464.zip")
-}
+$fileLocation64 = $(Join-Path (Split-Path -parent $toolsDir) "application\tftpd64.464.zip")
 
-Get-ChocolateyUnzip -fileFullPath $fileLocation -destination $toolsDir
+Get-ChocolateyUnzip -fileFullPath $fileLocation -FileFullPath64 $fileLocation64 -destination $toolsDir
