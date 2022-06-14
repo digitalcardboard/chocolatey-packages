a5a19b5ca526c44952d693f92f833fdc7da543f8
Jason Navarrete <jason.navarrete@allanbrosfruit.com>
2022-06-14 14:11:25 -0700

    iperf3 and xnview update

--- manual/iperf3/tools/chocolateyinstall.ps1
+++ manual/iperf3/tools/chocolateyinstall.ps1
@@ -1,11 +1,9 @@
 $ErrorActionPreference = 'Stop';
 
 $packageName = 'iPerf3'
-$url = 'https://iperf.fr/download/windows/iperf-3.1.3-win32.zip'
-$url64 = 'https://iperf.fr/download/windows/iperf-3.1.3-win64.zip'
-$checksum = '36229bacfda9ad0a05d7eba9e3843b18b94046da'
-$checksum64 = 'c63d306daac82987839ed94cd8705a1121af0181'
-$checksumType = 'sha1'
+$url64 = 'https://files.budman.pw/iperf3.11_64bit.zip'
+$checksum64 = '166f815beec86de38cc26c243c344c67143575f380974ee0033c6cef90398f84'
+$checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-Install-ChocolateyZipPackage -packageName "$packageName" -url "$url" -unzipLocation "$toolsDir" -checksum $checksum -checksumType $checksumType -url64bit "$url64" -checksum64 $checksum64
+Install-ChocolateyZipPackage -packageName "$packageName" -unzipLocation "$toolsDir" -checksum $checksum -checksumType $checksumType -url64bit "$url64" -checksum64 $checksum64
